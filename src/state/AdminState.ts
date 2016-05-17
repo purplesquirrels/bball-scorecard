01c1a9ed373a5ea544760bc6153d10cd604aa88c
bfoster <bfoster@c-learning.com.au>
2016-05-17 11:01:38 +1000

    added send test email button to admin

--- src/state/AdminState.ts
+++ src/state/AdminState.ts
@@ -2,12 +2,16 @@
 
 class AdminState extends AppState {
 
+	emailTimer: number;
+
 	
 	constructor(controller: ScoreController, app: App, options:any) {
 		super(controller, app, options);
 	}
 
 	render(renderoptions?:any) {
+
+		this.emailTimer = -1;
 
 		$(".app-header").addClass("hidden");
 
@@ -18,7 +22,6 @@ class AdminState extends AppState {
 		
 		var context = {
 		};
-
 
 
 		var html = template(context);
@@ -49,6 +52,51 @@ class AdminState extends AppState {
 			$("#first_name").val("");
 
 		});
+
+		$(".send-test-email").bind("click", (e) => {
+
+			if (this.emailTimer !== -1) return;
+
+			var email:string = $("#email").val();
+
+			var regex:RegExp = /^(?:(?:[\w`~!#$%^&*\-=+;:{}'|,?\/]+(?:(?:\.(?:"(?:\\?[\w`~!#$%^&*\-=+;:{}'|,?\/\.()<>\[\] @]|\\"|\\\\)*"|[\w`~!#$%^&*\-=+;:{}'|,?\/]+))*\.[\w`~!#$%^&*\-=+;:{}'|,?\/]+)?)|(?:"(?:\\?[\w`~!#$%^&*\-=+;:{}'|,?\/\.()<>\[\] @]|\\"|\\\\)+"))@(?:[a-zA-Z\d\-]+(?:\.[a-zA-Z\d\-]+)*|\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\])$/gi;
+			var result:string[] = regex.exec(email);
+
+			if (!result || !result[0]) return;
+
+			this.emailTimer = setTimeout(() => { this.emailTimer = -1}, 5000);
+
+			console.log("Sending test email");
+
+			var p_message = "<p>Hi test,</p><p>This is a test email sent from the Bankulator admin.</p>";
+
+			$.post(Config.MAIL_PATH, {
+				auth: Config.SERVER_KEY,
+				to: result[0],
+				subject: "Bankulator: test email",
+				message: p_message
+			}).then((e) => {
+
+				console.log("then", e);
+
+				$("#email").val("");
+
+			}).fail((e) => {
+
+				alert("There was an error sending email.");
+
+				console.log("fail", e);
+
+			}).always(() => {
+
+				if (this.emailTimer !== -1) {
+					clearTimeout(this.emailTimer);
+					this.emailTimer = -1;
+				}
+
+			});
+		})
+
 		$(".new-season").bind("click", (e) => {
 
 			var s = this.controller.getNewSeason();
@@ -73,8 +121,14 @@ class AdminState extends AppState {
 							var data = JSON.stringify(s);
 							var command = "create/season";
 
-							this.send(data, command).done((data) => { alert("New season created!"); })
-								.fail((data) => { alert("Error creating new season."); })
+							this.send(data, command).done((data) => {
+								console.log(data);
+								alert("New season created!");
+							})
+							.fail((data) => {
+								console.log(data);
+								alert("Error creating new season.");
+							})
 							.always((data) => { 
 								$('#newseason-modal').closeModal();
 							});
